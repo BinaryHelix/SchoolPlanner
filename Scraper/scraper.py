aeadbcb7eae361bb7b33da76b273a5cb9ddc824a
BinaryHelix <helix@openmailbox.org>
2014-01-14 19:50:54 -0800

    Added term loops (Summer, Fall, Spring) for debug
    Can't get courseDept to only take unique department names. End up with
    227 non-unique departments, 50 from summer, 89 from fall, 88 from
    spring.

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -4,8 +4,23 @@ import re           # for regex matching
 
 academicYear = 1314
 region = 'ALL'
+
 courseDept = []
+"""courseDeptSummer = []
+courseDeptFall = []
+courseDeptSpring = []"""
+
 a_courseNumber = []
+"""a_courseNumberSummer = []
+a_courseNumberFall = []
+a_courseNumberSpring = []"""
+
+a_courseTitle = []
+"""a_courseTitleSummer = []
+a_courseTitleFall = []
+a_courseTitleSpring = []"""
+
+a_courseInfo = []
 a_term = ['10', '20', '30']
 numOfTerms = len(a_term)
 
@@ -22,19 +37,41 @@ def getDepartmentList(region, academicYear, a_term):
         for line in data:
             regexDeptAll = re.compile('>([A-Z].......)<')
             for match in regexDeptAll.finditer(line.rstrip()):
-                if match:
+                if match.groups() not in courseDept:
+                    print match.groups()
                     courseDept.extend(match.groups())
-        return courseDept
+
+                    #TERM DEBUG
+                    """if a_term[termCount] == '10':
+                        courseDeptSummer.extend(match.groups())
+                    elif a_term[termCount] == '20':
+                        courseDeptFall.extend(match.groups())
+                    elif a_term[termCount] == '30':
+                        courseDeptSpring.extend(match.groups())"""
+
+    return courseDept
+    """return courseDeptSummer
+    return courseDeptFall
+    return courseDeptSpring"""
 
 getDepartmentList(region, academicYear, a_term)
 
-numOfDepts = len(courseDept)
+numOfDept = len(courseDept)
+"""numOfDeptSummer = len(courseDeptSummer)
+numOfDeptSpring = len(courseDeptSpring)
+numOfDeptFall = len(courseDeptFall)"""
+
+"""print numOfDeptSummer
+print numOfDeptSpring
+print numOfDeptFall"""
+print numOfDept
+
 
 
 #List all classes available for all Departments
-def getAllClasses(a_term, courseDept, region, academicYear):
+def getAllClasses(a_term, courseDept, courseDeptFall, courseDeptSpring, region, academicYear):
     for termCount in range (0,numOfTerms):
-        for courseDeptCount in range(0,numOfDepts):
+        for courseDeptCount in range (0,numOfDept):
             urlDept = 'http://classes.deltacollege.edu/classSchedule/courses.cfm?term=' + a_term[termCount] + '&year=' + '20' + str(academicYear) + '&dept=' + courseDept[courseDeptCount] + "&region=" + region
             usock = urllib2.urlopen(urlDept)
             data = usock.read()
@@ -42,23 +79,118 @@ def getAllClasses(a_term, courseDept, region, academicYear):
             data = [line.strip() for line in data.split('\n') if line.strip()]
 
             for line in data:
-                regexDept = re.compile(courseDept + '....(\d\d\d).')
-                for match in regexDept.finditer(line.rstrip('<')):
+                regexDept = re.compile('[A-Z| ]{1,12}\d\d\d[A-Z|<]')
+                for match in regexDept.finditer(line.rstrip()):
                     if match:
                         a_courseNumber.extend(match.groups())
-            return a_courseNumber
+                regexName = re.compile('TD VALIGN="TOP">.({1,100})')
+                for match in regexName.finditer(line.rstrip()):
+                    if match:
+                        a_courseTitle.extend(match.groups())
 
-#getAllClasses(a_term, courseDept, region, academicYear)
+    return a_courseNumber
+    return a_courseTitle
 
-numOfCourses = len(a_courseNumber)
+    """for termCount in range (0,numOfTerms):
+        for courseDeptCount in range (0,numOfDeptSummer):
+            urlDept = 'http://classes.deltacollege.edu/classSchedule/courses.cfm?term=' + a_term[termCount] + '&year=' + '20' + str(academicYear) + '&dept=' + courseDeptSummer[courseDeptCount] + "&region=" + region
+            usock = urllib2.urlopen(urlDept)
+            data = usock.read()
+            usock.close()
+            data = [line.strip() for line in data.split('\n') if line.strip()]
 
-print numOfCourses
+            for line in data:
+                regexDept = re.compile('[A-Z| ]{1,12}\d\d\d[A-Z|<]')
+                for match in regexDept.finditer(line.rstrip()):
+                    if match:
+                        a_courseNumberSummer.extend(match.groups())
+                regexName = re.compile('TD VALIGN="TOP">.({1,100})')
+                for match in regexName.finditer(line.rstrip()):
+                    if match:
+                        a_courseTitleSummer.extend(match.groups())
+
+    return a_courseNumberSummer
+    return a_courseTitleSummer
+
+
+    for termCount in range (0,numOfTerms):
+        for courseDeptCount in range (0,numOfDeptFall):
+            urlDept = 'http://classes.deltacollege.edu/classSchedule/courses.cfm?term=' + a_term[termCount] + '&year=' + '20' + str(academicYear) + '&dept=' + courseDeptFall[courseDeptCount] + "&region=" + region
+            usock = urllib2.urlopen(urlDept)
+            data = usock.read()
+            usock.close()
+            data = [line.strip() for line in data.split('\n') if line.strip()]
+
+            for line in data:
+                regexDept = re.compile('[A-Z| ]{1,12}\d\d\d[A-Z|<]')
+                for match in regexDept.finditer(line.rstrip()):
+                    if match:
+                        a_courseNumberFall.extend(match.groups())
+                regexName = re.compile('TD VALIGN="TOP">.({1,100})')
+                for match in regexName.finditer(line.rstrip()):
+                    if match:
+                        a_courseTitleFall.extend(match.groups())
+
+
+    return a_courseNumberFall
+    return a_courseTitleFall
+
+
+
+    for termCount in range (0,numOfTerms):
+        for courseDeptCountSpring in range (0,numOfDeptSpring):
+            urlDept = 'http://classes.deltacollege.edu/classSchedule/courses.cfm?term=' + a_term[termCount] + '&year=' + '20' + str(academicYear) + '&dept=' + courseDeptSpring[courseDeptCountSpring] + "&region=" + region
+            usock = urllib2.urlopen(urlDept)
+            data = usock.read()
+            usock.close()
+            data = [line.strip() for line in data.split('\n') if line.strip()]
+
+            for line in data:
+                regexDept = re.compile('[A-Z| ]{1,12}\d\d\d[A-Z|<]')
+                for match in regexDept.finditer(line.rstrip()):
+                    if match:
+                        a_courseNumberSpring.extend(match.groups())
+                regexName = re.compile('TD VALIGN="TOP">.({1,100})')
+                for match in regexName.finditer(line.rstrip()):
+                    if match:
+                        a_courseTitleSpring.extend(match.groups())
+
+
+
+
+    return a_courseNumberSpring
+    return a_courseTitleSpring"""
+
+
+
+
+#getAllClasses(a_term, courseDeptSummer, courseDeptFall, courseDeptSpring, region, academicYear)
+
+"""numOfCourseSummer = len(a_courseNumberSummer)
+numOfCourseFall = len(a_courseNumberFall)
+numOfCourseSpring = len(a_courseNumberSpring)"""
+
+numOfCourse = len(a_courseNumber)
+
+"""numOfTitleSummer = len(a_courseTitleSummer)
+numOfTitleFall = len(a_courseTitleFall)
+numOfTitleSpring = len(a_courseTitleSpring)"""
+
+numOfTitle = len(a_courseTitle)
+
+"""print numOfCourseSummer
+print numOfCourseFall
+print numOfCourseSpring
+
+print numOfTitleSummer
+print numOfTitleFall
+print numOfTitleSpring"""
 
 #List individual listings for all classes in all Departments
 def getAllEntries(a_term, a_courseNumber, courseDept, region, academicYear):
     for termCount in range(0,numOfTerms):
-        for courseDeptCount in range(0,numOfDepts):
-            for courseNumCount in range(0,numOfCourses):
+        for courseDeptCount in range(0,numOfDept):
+            for courseNumCount in range(0,numOfCourse):
 
                 # Construct URL
                 urlClass = 'http://classes.deltacollege.edu/classSchedule/sections.cfm?term=' + a_term[termCount] + '&academicYear=' + str(academicYear) + '&region=' + region + '&courseId=' + courseDept[courseDeptCount] + '%20%20%20%20' + a_courseNumber[courseNumCount]
